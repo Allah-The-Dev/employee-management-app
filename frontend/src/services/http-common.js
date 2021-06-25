e9c62a2cf1f07d63fe450e80e574ae37320c9545
Neelesh Pandey <neelesh.kumar_pandey@nokia.com>
2021-06-26 01:59:04 +0530

    removed localhost url from frontend

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
   headers: {
     "Content-type": "application/json",
     ...authHeader()
